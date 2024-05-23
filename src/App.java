864734a56784d8f97721ef85b4be198f9636e085
Diego Fernando Lenis Delgado <161733208+Diego-F-Lenis-c137@users.noreply.github.com>
2024-05-23 15:45:22 -0500

    Determinar ganador
    +Selección de jugador & mostrar resultados (refactorizar, implementarlo en vista por consola y GUI)
    +determinar ganador ✊<🖐<✌<✊...
    +comprobación casos de error, elección incorrecta
    !el archivo App se ejecuta en ciclo para probar ejecución/ implementar menu para continuar o cerrar juego

--- src/App.java
+++ src/App.java
@@ -1,15 +1,21 @@
 import controlador.ControladorJuego;
 import modelo.Juego;
+
 import vista.VistaJuegoConsola;
 //import vista.VistaJuegoGUI;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Juego modelo = new Juego();
+        do {
+            Juego modelo = new Juego();
+            
+            modelo.vs();
+            modelo.DeterminarGanador();
+        } while (true);
         //Consola o GUi
-        VistaJuegoConsola vista = new VistaJuegoConsola();
+        //VistaJuegoConsola vista = new VistaJuegoConsola();
         //VistaJuegoConsola vista = new VistaJuegoGUI();
-        ControladorJuego controlador = new ControladorJuego(modelo, vista);
+        //ControladorJuego controlador = new ControladorJuego(modelo, vista);
         //controlador.mostrarInterfaz();
     }
 }
--- src/controlador/ControladorJuego.java
+++ src/controlador/ControladorJuego.java
@@ -2,6 +2,9 @@ package controlador;
 
 import modelo.Juego;
 import vista.VistaJuego;
+import vista.VistaJuegoConsola;
+import vista.VistaJuegoGUI;
+
 //import vista.VistaJuegoConsola;
 //import vista.VistaJuegoGUI;
 import java.awt.event.ActionEvent;
@@ -11,16 +14,24 @@ public class ControladorJuego implements ActionListener {
 
     public Juego juego;
     public VistaJuego vista;
+    public VistaJuegoConsola vistaConsola;
+    public VistaJuegoGUI vistaGUI;
     public Opciones opcion;
     public ControladorJuego(Juego juego, VistaJuego vista) {
         this.juego = juego;
         this.vista = vista;
     }
-
+    public ControladorJuego(Juego juego, VistaJuegoConsola vista) {
+        this.juego = juego;
+        this.vistaConsola = vistaConsola;
+    }
     public void mostrarInterfaz(){
         vista.mostrarVista(this);
     }
-
+    public ControladorJuego(Juego juego, VistaJuegoGUI vista) {
+        this.juego = juego;
+        this.vistaGUI = vistaGUI;
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
